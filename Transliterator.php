8609ba991d79993d5183a65f224d509dc16d9978
GplCart <gplcart.software@gmail.com>
2017-11-06 17:55:25 +0200

    Core API changes

--- Transliterator.php
+++ Transliterator.php
@@ -9,7 +9,8 @@
 
 namespace gplcart\modules\transliterator;
 
-use gplcart\core\Module;
+use gplcart\core\Module,
+    gplcart\core\Config;
 
 /**
  * Main class for Transliterator module
@@ -18,11 +19,11 @@ class Transliterator extends Module
 {
 
     /**
-     * Constructor
+     * @param Config $config
      */
-    public function __construct()
+    public function __construct(Config $config)
     {
-        parent::__construct();
+        parent::__construct($config);
     }
 
     /**
